92ada67e0d70774f1d98c21a7b99ea081448f581
Paul Conte <paul@audiblebuzz.com>
2016-07-13 15:02:55 -0700

    query TripsForRoute then TripDetails for each trip

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 var Client = require('node-rest-client').Client;
-
 var client = new Client();
 
-client.get("http://api.pugetsound.onebusaway.org/api/where/trips-for-route/1_100016.json?key=TEST", function (data, response) {
-	// parsed response body as js object
-	console.log(data);
-	// raw response
-	//console.log(response);
-});
+var tripsForRouteUrl;
+var tripDetailsUrl;
+
+var routeId = '1_100016';
+var trips = [];
+var tripId;
+var status;
+
+setInterval(function () {
+  console.log('========');
+  tripsForRouteUrl = `http://api.pugetsound.onebusaway.org/api/where/trips-for-route/${routeId}.json?key=TEST`;
+
+  client.get(tripsForRouteUrl, function (data, response) {
+    trips = data.data.references.trips;
+    console.log(data.currentTime, trips);
+
+    trips.forEach(function (trip) {
+      tripId = trip.id;
+      tripDetailsUrl = `http://api.pugetsound.onebusaway.org/api/where/trip-details/${tripId}.json?key=TEST`;
+
+      client.get(tripDetailsUrl, function (data, response) {
+        status = data.data.entry.status;
+        console.log(data.currentTime, status.activeTripId, status.lastKnownLocation);
+      });
+    });
+  });
+}, 1000);
