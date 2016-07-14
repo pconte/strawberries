694b29e7e9788fc33a0393b8d9928efd639261c1
Paul Conte <paul@audiblebuzz.com>
2016-07-14 12:20:43 -0700

    add winston package, log data to file using winston

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
+var winston = require('winston');
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'debug' }),
+    new (winston.transports.File)({
+      filename: 'data.log',
+      level: 'silly'
+    })
+  ]
+});
+
 var tripsForRouteUrl;
 var tripDetailsUrl;
 
@@ -8,9 +19,10 @@ var routeId = '1_100016'; // this is the routeId for route #118
 var trips = [];
 var tripId;
 var status;
+var row;
 
 setInterval(function () {
-  console.log('========');
+  //console.log('========');
   tripsForRouteUrl = `http://api.pugetsound.onebusaway.org/api/where/trips-for-route/${routeId}.json?key=TEST`;
 
   client.get(tripsForRouteUrl, function (data, response) {
@@ -23,7 +35,13 @@ setInterval(function () {
 
       client.get(tripDetailsUrl, function (data, response) {
         status = data.data.entry.status;
-        console.log(data.currentTime, status.activeTripId, status.lastKnownLocation);
+        //console.log(data.currentTime, status.activeTripId, status.lastKnownLocation);
+        row = {
+          time: data.currentTime,
+          tripId: status.activeTripId,
+          location: status.lastKnownLocation
+        };
+        logger.log('silly', JSON.stringify(row));
       });
     });
   });
