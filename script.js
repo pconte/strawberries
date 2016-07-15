09baab79ad5edc32b872a9290a4b8ae4d5b528b2
Paul Conte <paul@audiblebuzz.com>
2016-07-14 17:10:21 -0700

    grab data for both route 118 and route 119, add additional fields to row

--- script.js
+++ script.js
@@ -33,40 +33,46 @@ var logger = new (winston.Logger)({
 var tripsForRouteUrl;
 var tripDetailsUrl;
 
-var routeId = '1_100016'; // this is the routeId for route #118
+var routeIds = ['1_100016', '1_100017']; // these are the routeIds for routes #118 and #119
 var trips = [];
 var tripId;
 var status;
 
 var row;
 
-setInterval(function () {
-  tripsForRouteUrl = `http://api.pugetsound.onebusaway.org/api/where/trips-for-route/${routeId}.json?key=TEST`;
+routeIds.forEach(function (routeId) {
+  setInterval(function () {
+    tripsForRouteUrl = `http://api.pugetsound.onebusaway.org/api/where/trips-for-route/${routeId}.json?key=TEST`;
 
-  client.get(tripsForRouteUrl, function (data, response) {
-    trips = data.data.references.trips;
+    client.get(tripsForRouteUrl, function (data, response) {
+      trips = data.data.references.trips;
 
-    trips.forEach(function (trip) {
-      tripId = trip.id;
-      tripDetailsUrl = `http://api.pugetsound.onebusaway.org/api/where/trip-details/${tripId}.json?key=TEST`;
+      trips.forEach(function (trip) {
+        tripId = trip.id;
+        tripDetailsUrl = `http://api.pugetsound.onebusaway.org/api/where/trip-details/${tripId}.json?key=TEST`;
 
-      client.get(tripDetailsUrl, function (data, response) {
-        status = data.data.entry.status;
-        row = {
-          time: data.currentTime,
-          tripId: status.activeTripId,
-          location: status.lastKnownLocation
-        };
-        logger.data(JSON.stringify(row));
+        client.get(tripDetailsUrl, function (data, response) {
+          status = data.data.entry.status;
+          row = {
+            time: data.currentTime,
+            routeId: routeId,
+            activeTripId: status.activeTripId,
+            status: status.status,
+            lastKnownLocation: status.lastKnownLocation,
+            position: status.position,
+            vehicleId: status.vehicleId
+          };
+          logger.data(JSON.stringify(row));
+        });
       });
     });
-  });
-}, 1000);
+  }, 1000);
+});
 
 /*
 TODO:
 - get a list of all routes that are currently running
-- or alternatively, directly get all vehicles currently running (and derive rest of the data from the vehicle)
-- modify the calls that are made (but it seems like the basic nest flow inside a SetInterval will still work?)
-
+- or alternatively, directly get all vehicles currently running (and derive rest of the data from the vehicles)
+- likely change the API requests (but it seems like the nested flow inside a SetInterval will still work?)
+- how often should the Interval loop run?
 */
