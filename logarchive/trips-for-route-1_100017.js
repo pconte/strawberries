2f40c6b9031f6b55f893c3a7a54a359fcdcf27d9
EMMETT PICKEREL <emmett@EMMETTs-MacBook-Air.local>
2016-07-22 16:57:51 -0700

    Logs for today, plus trips for route with more than one route

---
+++ logarchive/trips-for-route-1_100017.js
@@ -0,0 +1,229 @@
+{
+	code: 200,
+	currentTime: 1469231223528,
+	data: {
+		limitExceeded: false,
+		list: [
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_30899750"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_31508861"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_31508901"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_31508917"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_30899737"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_31509121"
+			},
+			{
+				frequency: null,
+				schedule: null,
+				serviceDate: 1469170800000,
+				situationIds: [ ],
+				status: null,
+				tripId: "1_31508878"
+			}
+		],
+		outOfRange: false,
+		references: {
+			agencies: [
+				{
+					disclaimer: "",
+					email: "",
+					fareUrl: "",
+					id: "1",
+					lang: "EN",
+					name: "Metro Transit",
+					phone: "206-553-3000",
+					privateService: false,
+					timezone: "America/Los_Angeles",
+					url: "http://metro.kingcounty.gov"
+				}
+			],
+			routes: [
+				{
+					agencyId: "1",
+					color: "",
+					description: "Highline College to Marine View to BurienTC to CBD",
+					id: "1_100020",
+					longName: "",
+					shortName: "121",
+					textColor: "",
+					type: 3,
+					url: "http://metro.kingcounty.gov/schedules/121/n0.html"
+				},
+				{
+					agencyId: "1",
+					color: "",
+					description: "Dockton to Vashon to Downtown Seattle",
+					id: "1_100017",
+					longName: "",
+					shortName: "119",
+					textColor: "",
+					type: 3,
+					url: "http://metro.kingcounty.gov/schedules/119/n0.html"
+				},
+				{
+					agencyId: "1",
+					color: "",
+					description: "Fauntleroy Ferry to Downtown Seattle",
+					id: "1_100014",
+					longName: "",
+					shortName: "116",
+					textColor: "",
+					type: 3,
+					url: "http://metro.kingcounty.gov/schedules/116/n0.html"
+				},
+				{
+					agencyId: "1",
+					color: "",
+					description: "Highline College to Burien TC to CBD",
+					id: "1_100021",
+					longName: "",
+					shortName: "122",
+					textColor: "",
+					type: 3,
+					url: "http://metro.kingcounty.gov/schedules/122/n0.html"
+				},
+				{
+					agencyId: "1",
+					color: "",
+					description: "Tahlequah to Vashon to Downtown Seattle",
+					id: "1_100016",
+					longName: "",
+					shortName: "118",
+					textColor: "",
+					type: 3,
+					url: "http://metro.kingcounty.gov/schedules/118/n0.html"
+				}
+			],
+			situations: [ ],
+			stops: [ ],
+			trips: [
+				{
+					blockId: "1_4230676",
+					directionId: "1",
+					id: "1_30899750",
+					routeId: "1_100020",
+					routeShortName: "",
+					serviceId: "1_62763",
+					shapeId: "1_10121074",
+					timeZone: "",
+					tripHeadsign: "DOWNTOWN SEATTLE VIA SR-509",
+					tripShortName: "LOCAL"
+				},
+				{
+					blockId: "1_4218525",
+					directionId: "1",
+					id: "1_31508861",
+					routeId: "1_100017",
+					routeShortName: "",
+					serviceId: "1_62763",
+					shapeId: "1_10119016",
+					timeZone: "",
+					tripHeadsign: "FERRY TERMINAL VIA SHUTTLE",
+					tripShortName: "LOCAL"
+				},
+				{
+					blockId: "1_4218529",
+					directionId: "0",
+					id: "1_31508901",
+					routeId: "1_100017",
+					routeShortName: "119E",
+					serviceId: "1_62763",
+					shapeId: "1_21119027",
+					timeZone: "",
+					tripHeadsign: "VASHON ISLAND - Express",
+					tripShortName: "EXPRESS"
+				},
+				{
+					blockId: "1_4218430",
+					directionId: "0",
+					id: "1_31508917",
+					routeId: "1_100014",
+					routeShortName: "116E",
+					serviceId: "1_62763",
+					shapeId: "1_21116022",
+					timeZone: "",
+					tripHeadsign: "FAUNTLEROY",
+					tripShortName: "EXPRESS"
+				},
+				{
+					blockId: "1_4230669",
+					directionId: "0",
+					id: "1_30899737",
+					routeId: "1_100021",
+					routeShortName: "",
+					serviceId: "1_62763",
+					shapeId: "1_21122021",
+					timeZone: "",
+					tripHeadsign: "HIGHLINE COLLEGE VIA SR-509",
+					tripShortName: "LOCAL"
+				},
+				{
+					blockId: "1_4218530",
+					directionId: "1",
+					id: "1_31509121",
+					routeId: "1_100016",
+					routeShortName: "",
+					serviceId: "1_62763",
+					shapeId: "1_10118346",
+					timeZone: "",
+					tripHeadsign: "FERRY TERMINAL VIA SHUTTLE",
+					tripShortName: "LOCAL"
+				},
+				{
+					blockId: "1_4218524",
+					directionId: "0",
+					id: "1_31508878",
+					routeId: "1_100016",
+					routeShortName: "",
+					serviceId: "1_62763",
+					shapeId: "1_21118340",
+					timeZone: "",
+					tripHeadsign: "BURTON",
+					tripShortName: "LOCAL"
+				}
+			]
+		}
+	},
+	text: "OK",
+	version: 2
+}
